676f45e037a1f4a056b361cbf6865a334490c367
Stefan <stefan.blos@gmail.com>
2024-11-05 19:24:47 +0100

    Initial Commit

---
+++ MyAppleIntelligence/MyAppleIntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppleIntelligenceApp.swift
+//  MyAppleIntelligence
+//
+//  Created by Stefan Blos on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MyAppleIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
